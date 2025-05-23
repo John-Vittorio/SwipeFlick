778cc8601fcff4521c7cb8ba9e80525a55a970d4
dominionism <cainglet@uw.edu>
2025-05-23 00:41:03 -0700

    Added primary view controllers, views, and segues.

---
+++ SwipeFlick/SwipeFlick/Controllers/SwipeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SwipeViewController.swift
+//  SwipeFlick
+//
+//  Created by Abdul Wahid on 5/23/25.
+//
+
+import UIKit
+
+class SwipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SwipeFlick/SwipeFlickTests/SwipeFlickTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeFlickTests.swift
+//  SwipeFlickTests
+//
+//  Created by Abdul Wahid on 5/23/25.
+//
+
+import Testing
+@testable import SwipeFlick
+
+struct SwipeFlickTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
