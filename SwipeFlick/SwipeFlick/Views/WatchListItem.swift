c66b2571f868a1b0476728e12c42c7fda948ba40
dominionism <cainglet@uw.edu>
2025-05-24 22:19:56 -0700

    Trying to add a xib.

---
+++ SwipeFlick/SwipeFlick/Views/WatchListItem.swift
@@ -0,0 +1,19 @@
+//
+//  WatchListItem.swift
+//  SwipeFlick
+//
+//  Created by Abdul Wahid on 5/24/25.
+//
+
+import UIKit
+
+class WatchListItem: UICollectionViewCell {
+    @IBOutlet weak var moviePoster: UIImageView!
+    @IBOutlet weak var moveTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
